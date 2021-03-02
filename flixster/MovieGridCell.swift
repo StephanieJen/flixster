e8d9b51f0a9663826ecdf93795e7108b0ef98213
StephanieJen <69771477+StephanieJen@users.noreply.github.com>
2021-03-01 23:52:55 -0500

    asmt2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Stephanie Jen  on 2021/3/1.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
